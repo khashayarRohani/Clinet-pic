47ea27b8459dfee15e515931297f9824bef50815
khashayarRohani <khashayarrhi@gmail.com>
2024-07-23 13:22:49 +0100

    new fetch

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const imageContainer = document.getElementById("ImageContainer");
 async function getImages() {
-  const response = await fetch("http://localhost:8080/images");
+  const response = await fetch("https://server-pic.onrender.com");
   const Imgdata = await response.json();
   const keys = Object.keys(Imgdata);
   for (let i = 0; i < keys.length; i++) {
